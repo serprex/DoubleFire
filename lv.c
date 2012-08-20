30ad6d6e8982aada2d2e604797799d5c8f7fc567
Demur Rumed <gunkmute@gmail.com>
2012-08-20 23:56:40 +0000

    Since w8/w16/wfloat are static now lv can also have such static declarations

--- lv.c
+++ lv.c
@@ -1,35 +1,42 @@
 #include "df.h"
 static uint8_t Lv[65536];
 uint8_t*Lp=Lv;
-#define l(x) static void l##x(uint##x##_t a){*(uint##x##_t*)Lp=a;Lp+=x/8;}static int##x##_t rl##x(){Lp+=x/8;return*(int##x##_t*)(Lp-x/8);}
-#define lt(t) static void l##t(t a){*(t*)Lp=a;Lp+=sizeof(t);}static t rl##t(){Lp+=sizeof(t);return*(t*)(Lp-sizeof(t));}
-l(8)
-l(16)
-lt(float)
+#define w(x) static void w##x(uint##x##_t a){*(uint##x##_t*)Lp=a;Lp+=x/8;}static int##x##_t r##x(){Lp+=x/8;return*(int##x##_t*)(Lp-x/8);}
+#define wt(t) static void w##t(t a){*(t*)Lp=a;Lp+=sizeof(t);}static t r##t(){Lp+=sizeof(t);return*(t*)(Lp-sizeof(t));}
+w(8)
+w(16)
+wt(float)
 #define PBIT (p?128:0)
 #define E(x) static void x(uint16_t t
 #define P ,int p
 #define XY ,int16_t x,int16_t y
 #define VD ,float v,float d
-#define T(x) ){l16(t);l8(x);
+#define T(x) ){w16(t);w8(x);
 #define END }
-#define WVD lfloat(v);lfloat(d);
-#define WXY l16(x);l16(y);
-#define RXY e->x=rl16();e->y=rl16();
-#define RVD v=rlfloat();d=rlfloat();
-#define RF2(v,d) v=rlfloat();d=rlfloat();
+#define WVD wfloat(v);wfloat(d);
+#define WXY w16(x);w16(y);
+#define RXY e->x=r16();e->y=r16();
+#define RVD v=rfloat();d=rfloat();
+#define RF2(v,d) v=rfloat();d=rfloat();
 #define EXYV e->xd=cos(d)*v;e->yd=sin(d)*v;
+E(can) XY VD T(CAN) WXY WVD END
+E(tar) XY T(TAR) WXY END
+E(b1) T(B1) END
+E(b2) T(B2) END
+E(b3) P T(PBIT|B3) END
+E(rot) P XY VD T(PBIT|ROT) WXY WVD END
+E(dog) XY VD T(DOG) WXY WVD END
+E(poo) XY VD T(POO) WXY WVD END
 void mke(){
 	if(T==*(uint16_t*)Lp){
 		float v,d;
 		uint8_t*LL=Lp;
 		Lp+=2;
-		uint8_t t=rl8();
 		obje*e=Etop;
-		e->t=t;
+		e->t=r8();
 		e->c=T;
-		switch(t&127){
-		default:printf("Unknown E%x\n",t);
+		switch(e->t&127){
+		default:printf("Unknown E%x\n",e->t);
 		case(CAN)
 			e->h=5;
 			RXY
@@ -70,14 +77,6 @@ void mke(){
 		incE(Lp-LL);
 	}
 }
-E(can) XY VD T(CAN) WXY WVD END
-E(tar) XY T(TAR) WXY END
-E(b1) T(B1) END
-E(b2) T(B2) END
-E(b3) P T(PBIT|B3) END
-E(rot) P XY VD T(PBIT|ROT) WXY WVD END
-E(dog) XY VD T(DOG) WXY WVD END
-E(poo) XY VD T(POO) WXY WVD END
 void genL1(){
 	for(int i=0;i<5;i++){
 		can(i*60+5,10,246,1,M_PI*3/2);
